96e17d99f8f359b4dcdf9ede9b5c623820414d79
Orlov Sergey <orlov.home@gmail.com>
2012-04-12 08:16:18 +0400

    first commit

---
+++ src/ru/cyberwasp/teamsplit/Team.java
@@ -0,0 +1,13 @@
+package ru.cyberwasp.teamsplit;
+
+import java.util.List;
+
+public class Team {
+	
+	public static List<Team> split(List<Player> players)
+	{
+		for (Player player : players) {
+			
+		}
+	}
+}
