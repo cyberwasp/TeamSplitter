c7717ef665da88d5a89e9477a3aa0779fb0ce47d
Orlov Sergey <orlov.home@gmail.com>
2012-11-05 15:59:10 +0400

    New app icon

---
+++ make-drawable.bat
@@ -0,0 +1,4 @@
+inkscape -f teamsplitter.svg -w 32 -h 32 -e res\drawable-hdpi\ic_launcher.png 
+inkscape -f teamsplitter.svg -w 36 -h 36 -e res\drawable-ldpi\ic_launcher.png 
+inkscape -f teamsplitter.svg -w 48 -h 48 -e res\drawable-mdpi\ic_launcher.png 
+inkscape -f teamsplitter.svg -w 96 -h 96 -e res\drawable-xhdpi\ic_launcher.png 
